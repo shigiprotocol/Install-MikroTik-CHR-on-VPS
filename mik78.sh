f520b14d87641c6456bb56d381fc765f79467a93
shigiprotocol <121646194+shigiprotocol@users.noreply.github.com>
2026-01-29 16:57:41 +0100

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -6,7 +6,7 @@ echo "=== https://github.com/azadrahorg ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
-wget https://download.mikrotik.com/routeros/7.20.7/chr-7.20.7.img.zip -O chr.img.zip  && \
+wget https://download.mikrotik.com/routeros/7.20.7/chr-7.20.7.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
